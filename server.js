de5e92657f05f028f0019be7c30dc5837a07489f
James <bakerje@mac.com>
2018-11-27 19:42:21 +0000

    Started server config

--- server.js
+++ server.js
@@ -1,7 +1,35 @@
 const express = require('express')
+const hbs = require('hbs')
 
 var app = express();
 
-app.get('/', () => {
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
 
+app.get('/', (req, res) => {
+	//res.send('<h1>Hello Express!</h1>');
+	res.send({
+		name: 'James',
+		likes: [
+			'coding',
+			'golf'
+		]
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page',
+		currentYear: new Date().getFullYear()
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		errorMessage: 'Error'	
+	})
+});
+
+app.listen(3000, () => {
+	console.log('Server is up on port 3000')
 });
