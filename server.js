39fa589cc917264d98b4e2a31e3e1d8d606a469b
James <bakerje@mac.com>
2018-11-29 18:30:59 +0000

    Finished first static web page

--- server.js
+++ server.js
@@ -7,13 +7,10 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-	//res.send('<h1>Hello Express!</h1>');
-	res.send({
-		name: 'James',
-		likes: [
-			'coding',
-			'golf'
-		]
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome to this website',
+		currentYear: new Date().getFullYear()
 	});
 });
 
