ba97f56b296a82094f6c9f47e42ebd26c72c38dd
James <bakerje@mac.com>
2018-12-04 11:43:49 +0000

    New page for Projects

--- server.js
+++ server.js
@@ -47,6 +47,13 @@ app.get('/about', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Projects Page',
+		pageDetails: 'Some examples of my work on GitHub'
+	});
+});
+
 app.get('/bad', (req, res) => {
 	res.send({
 		errorMessage: 'Error'	
